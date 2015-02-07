b69a4676fc6289ead49af57751d62eed069d32fc
jeon97 <j.jeon971@gmail.com>
2015-02-06 20:33:08 -0500

    init

---
+++ canvas-iPad/canvas-iPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  canvas-iPad
+//
+//  Created by Jaehyun Jeon on 2/6/15.
+//  Copyright (c) 2015 Jaehyun Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
